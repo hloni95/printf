525325be12a65fa147b78ca133baad1e2cd66c50
hloni95 <mltau95@gmail.com>
2023-07-25 14:10:45 -0700

    custom printf function

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_printf(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    char ch;
+    for (size_t i = 0; format[i] != '\0'; ++i) {
+        if (format[i] == '%') {
+            ++i;
+            switch (format[i]) {
+                case 'c':
+                    ch = va_arg(args, int);
+                    putchar(ch);
+                    break;
+                case 's':
+                    puts(va_arg(args, const char*));
+                    break;
+                default:
+                    putchar('%');
+                    putchar(format[i]);
+            }
+        } else {
+            putchar(format[i]);
+        }
+    }
+
+    va_end(args);
+}
