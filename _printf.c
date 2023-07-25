c68a4a29748636e776e4a83eb99521a6cd9aac63
hloni95 <mltau95@gmail.com>
2023-07-25 15:51:23 -0700

    message

--- _printf.c
+++ _printf.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void my_printf(const char* format, ...) {
+void my_printf(const char* c, ...)
+{
     va_list args;
     va_start(args, format);
 
     char ch;
-    for (size_t i = 0; format[i] != '\0'; ++i) {
-        if (format[i] == '%') {
+    for (size_t i = 0; format[i] != '\0'; ++i)
+    {
+        if (format[i] == '%')
+	{
             ++i;
-            switch (format[i]) {
+            switch (format[i]) 
+	    {
                 case 'c':
                     ch = va_arg(args, int);
                     putchar(ch);
@@ -21,7 +25,9 @@ void my_printf(const char* format, ...) {
                     putchar('%');
                     putchar(format[i]);
             }
-        } else {
+        }
+	else
+	{
             putchar(format[i]);
         }
     }
