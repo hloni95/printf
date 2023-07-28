688e9d91328748e0b03d57cc571252b558827fc8
hloni95 <mltau95@gmail.com>
2023-07-28 08:35:01 -0700

    printf function

--- _printf.c
+++ _printf.c
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdarg.h>
-
-void my_printf(const char* c, ...)
-{
-    va_list args;
-    va_start(args, format);
-
-    char ch;
-    for (size_t i = 0; format[i] != '\0'; ++i)
-    {
-        if (format[i] == '%')
-	{
-            ++i;
-            switch (format[i]) 
-	    {
-                case 'c':
-                    ch = va_arg(args, int);
-                    putchar(ch);
-                    break;
-                case 's':
-                    puts(va_arg(args, const char*));
-                    break;
-                default:
-                    putchar('%');
-                    putchar(format[i]);
-            }
-        }
-	else
-	{
-            putchar(format[i]);
-        }
-    }
-
-    va_end(args);
-=======
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
@@ -100,5 +63,4 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
->>>>>>> c12d382f97917415eae134c8b4ae2e94e4bf3318
 }
